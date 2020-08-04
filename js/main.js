e084d76765e300a31d092c5652892c06fbe9bd95
Josh Nussbaum <Joshnussbaum89@gmail.com>
2020-08-04 17:43:02 -0400

    add locations section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 /* ============================================= */
 /*              JavaScript                       */
 /* ============================================= */
+
+const search = document.querySelector('#search');
+
+// Hide location icon when focused
+search.addEventListener('focusin', () => {
+    document.querySelector('.locationmarker').classList.toggle('hide-content');
+});
+
+// Show location icon when not in focus
+search.addEventListener('focusout', function () {
+    document.querySelector('.locationmarker').classList.toggle('hide-content');
+});
