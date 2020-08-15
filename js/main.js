13cb6b49a06545ec4edffbd6af5affe3e8e28c4c
Josh Nussbaum <Joshnussbaum89@gmail.com>
2020-08-15 15:58:01 -0400

    finish layout

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 /* ============================================= */
 
 const search = document.querySelector('#search');
+const send = document.querySelector('.contact-form button');
 
 // Hide location icon when focused
 search.addEventListener('focusin', () => {
@@ -12,4 +13,9 @@ search.addEventListener('focusin', () => {
 // Show location icon when not in focus
 search.addEventListener('focusout', function () {
     document.querySelector('.locationmarker').classList.toggle('hide-content');
+});
+
+// Alert user to confirm send
+send.addEventListener('click', () => {
+    alert('Are you sure you want to send?');
 });
